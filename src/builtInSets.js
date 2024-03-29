34d699ab2a0aae5d17735e603ee36b86ac221d96
GDiamond778 <67438917+GDiamond778@users.noreply.github.com>
2024-03-29 02:28:39 -0400

    Add files via upload

--- src/builtInSets.js
+++ src/builtInSets.js
@@ -1,1 +1,57 @@
-var builtInSets = [];
+var builtInSets = [
+    {
+	    name: "Azulon",
+	    setName: "bane of my existence",
+	    evs: {
+	        hp: 2,
+	        energy: 0,
+	        attack: 5,
+	        defense: 0,
+	        attackR: 0,
+	        defenseR: 0,
+	        speed: 5
+	    },
+	    moves: {
+	        move1: "Draining Fang",
+	        move2: "Draining Fang",
+	        move3: "Draining Fang",
+	        move4: "Draining Fang"
+	    },
+	    posNature: "brawny",
+	    negNature: "none",
+	    ability: "None",
+	    item: "Wood Bracelet",
+	    level: 50
+	},
+
+    {
+	    name: "Kraboo",
+	    setName: "calc breaker",
+	    evs: {
+	        hp: 5,
+	        energy: 0,
+	        attack: 0,
+	        defense: 5,
+	        attackR: 0,
+	        defenseR: 2,
+	        speed: 0
+	    },
+	    moves: {
+	        move1: "Strike",
+	        move2: "Strike",
+	        move3: "Strike",
+	        move4: "Strike"
+	    },
+	    posNature: "robust",
+	    negNature: "none",
+	    ability: "None",
+	    item: "Sap Seed",
+	    level: 50
+	},
+
+
+
+
+
+
+];
